02023e41fdfefe6591edbbfe47991e2589a82e16
Akhil Mahesh <akhilmahesh@proton.me>
2024-11-14 14:37:07 +0530

    Create basicArithmetics.dart

---
+++ dart/dart-001/basicArithmetics.dart
@@ -0,0 +1,22 @@
+import 'dart:math'; 
+
+void main() {
+  print(2+2);
+  print(2-2);
+  print(2*2);
+  print(2/2);
+  print(2/0);
+  print(0/2);
+  print(pow(2,4)); // Works only if the math module is imported
+}
+
+/*
+ * Outputs
+ * 4
+ * 0
+ * 4
+ * 1
+ * Infinity
+ * 0
+ * 16
+*/
