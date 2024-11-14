6d3dc1f1a3853affad8edc446c43f3af56ea8bb3
Akhil Mahesh <akhilmahesh@proton.me>
2024-11-14 14:58:50 +0530

    Create attributes and methods.dart

---
+++ dart/dart-001/attributes and methods.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Attributes
+  var myName = 'AlonePhilic';
+  print(myName);
+  print(myName.length);
+  
+  // Methods
+  print('\n'+myName.toUpperCase());
+  print(myName.toLowerCase());
+}
+
+/*
+  AlonePhilic
+  11
+
+  ALONEPHILIC
+  alonephilic
+*/
