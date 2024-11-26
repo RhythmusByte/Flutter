5d0abaf6b0850ee977304a99f1d5102b9f74b58f
RhythmusByte <akhilmahesh@proton.me>
2024-11-26 20:26:07 +0530

    Execise 01

--- execises/01-numbers.dart
+++ execises/01-numbers.dart
@@ -1,14 +1,16 @@
 void main() {
-//  Given the double below, assign it 
-//  to a new variable where its rounded to the 
-//  nearest integer. 
+  //  Given the double below, assign it 
+  //  to a new variable where its rounded to the 
+  //  nearest integer. 
   
   var someNum = 3.1;
   int num = someNum.round();
   print(num);
 
-// BONUS QUESTION; In the cases of 0.5, how can you
-// ensure its rounded up or down?
-  double a = 0.5;
+  // BONUS QUESTION; In the cases of 0.5, how can you
+  // ensure its rounded up or down?
+  double a = 7.5;
   print(a.round());
+  print(a.ceil()); // Go up
+  print(a.floor()); // Go down
 }
