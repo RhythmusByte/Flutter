4b5339ea80e11a15f079e37f1afa7f19dd9de6be
RhythmusByte <akhilmahesh@proton.me>
2024-11-26 20:51:12 +0530

    Strings

---
+++ dart/strings/string.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = 'Akhil Mahesh';
+  print('My name is $name');
+  print("Length of my name is ${name.length}");
+
+  int money = 1200;
+  print('I have \$$money in bank');
+}
