69c204c0338ca260397efa24f6e7759e65259f9f
RhythmusByte <akhilmahesh@proton.me>
2024-11-26 23:24:20 +0530

    Excercise 03

---
+++ exercises/03-list.dart
@@ -0,0 +1,9 @@
+void main() {
+//  Show two different methods of
+// grabbing the last item in the list
+  var myList = ['A','B','C','D','E'];
+  int lastIndex = myList.length - 1;
+  print(myList);
+  print(myList[lastIndex]);
+}
+ 
