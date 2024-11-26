d9e01d0e121af43024908b4ad0b3f784413cd1a5
RhythmusByte <akhilmahesh@proton.me>
2024-11-26 21:08:01 +0530

    Exercise: Strings

---
+++ execises/02-string.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Given the string below, replace all the
+  // "x"s with a blank string
+  var errorMessage = "Uxh oxh, thexre's a bunxch of txypos";
+  String correctedMessage = errorMessage.replaceAll('x', '');
+  print(correctedMessage);
+}
